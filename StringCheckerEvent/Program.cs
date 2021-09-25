e96a4c627d54194f8bcfbad8a5a94740274e62f8
nur-987 <76473637+nur-987@users.noreply.github.com>
2021-09-25 16:48:43 +0800

    Add project files.

---
+++ StringCheckerEvent/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCheckerEvent
+{
+    /// <summary>
+    /// 4 .Write a C# program to create a function to input a string 
+    /// and count number of words are present  in the string , 
+    /// then  print them separately in an event.
+    /// </summary>
+    public delegate void stringCounting(int num);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type your sentence:");
+            string input = Console.ReadLine();
+
+            StringHelper myWord = new StringHelper();
+
+            myWord.counterEvent += MyWord_counterEvent1;
+            myWord.Count(input);
+
+            Console.ReadLine();
+        }
+
+        private static void MyWord_counterEvent1(int num)
+        {
+            int answ = num;
+            Console.WriteLine("The total num of words in the sentence are: " + answ);
+        }
+    }
+}
---
+++ StringCheckerEvent/StringHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCheckerEvent
+{
+    class StringHelper
+    {
+        public event stringCounting counterEvent;
+
+        public void Count(string str)
+        {
+
+            string[] words = str.Split(' ');
+            int wordCount = words.Length;
+
+            if (counterEvent != null)
+            {
+                counterEvent.Invoke(wordCount);
+            }
+
+        }
+
+    }
+    
+}
