42d104a619dd4a3b4d1db3df5033a16966ca7074
nur-987 <76473637+nur-987@users.noreply.github.com>
2021-09-26 09:35:30 +0800

    Hcf and Lcm with events

---
+++ HcfLcmEvents/HcmLcmChecker.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HcfLcmEvents
+{
+    class HcmLcmChecker
+    {
+        public event operation CalculationHcfComplete;
+        public event operation CalculationLcmComplete;
+
+        public void FindHcf(int a, int b)
+        {
+            //action to calculate HCF
+            int hcf = 0;
+            int j = (a < b) ? a : b;
+            for (int i = 1; i <= j; i++)
+            {
+                if (a % i == 0 && b % i == 0)
+                {
+                    hcf = i;
+                }
+            }
+
+            if (CalculationHcfComplete != null)
+            {
+                CalculationHcfComplete.Invoke(hcf);
+            }
+        }
+
+        public void FindLcm(int a, int b)
+        {
+            //action to calculate HCF
+            int x, y;
+            int lcm;
+
+            if (a > b)
+            {
+                x = a;
+                y = b;
+            }
+            else
+            {
+                x = b;
+                y = a;
+            }
+            for (int i = 1; i <= y; i++)
+            {
+                if ((x * i) % y == 0)
+                {
+                    lcm = x * i;
+                }
+            }
+            lcm = y;
+
+            if (CalculationLcmComplete != null)
+            {
+                CalculationLcmComplete.Invoke(lcm);
+            }
+        }
+
+    }
+}
---
+++ HcfLcmEvents/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HcfLcmEvents
+{
+    delegate void operation(int num);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the first numbers to check");
+            int num1 = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the second numbers to check");
+            int num2 = Int32.Parse(Console.ReadLine());
+
+            HcmLcmChecker myNumber = new HcmLcmChecker();
+            myNumber.CalculationHcfComplete += MyNumber_CalculationHcfComplete;
+            myNumber.CalculationLcmComplete += MyNumber_CalculationLcmComplete;
+
+            myNumber.FindHcf(num1, num2);
+            myNumber.FindLcm(num1, num2);
+
+            Console.ReadLine();
+        }
+
+        private static void MyNumber_CalculationLcmComplete(int num)
+        {
+            Console.WriteLine("HCF of the 2 numbers is: " + num);
+        }
+
+        private static void MyNumber_CalculationHcfComplete(int num)
+        {
+            Console.WriteLine("LCM of the 2 numbers is: " + num);
+        }
+    }
+}
